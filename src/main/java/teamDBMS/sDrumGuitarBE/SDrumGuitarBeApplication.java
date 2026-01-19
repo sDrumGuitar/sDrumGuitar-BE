f92c1b2bbc4491266f22b8f2b91851994870c509
seizethedaylyn <rlacofls294@gmail.com>
2026-01-19 19:59:20 +0900

    chore: init project

---
+++ src/main/java/teamDBMS/sDrumGuitarBE/SDrumGuitarBeApplication.java
@@ -0,0 +1,13 @@
+package teamDBMS.sDrumGuitarBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDrumGuitarBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SDrumGuitarBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamDBMS/sDrumGuitarBE/SDrumGuitarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package teamDBMS.sDrumGuitarBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SDrumGuitarBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
