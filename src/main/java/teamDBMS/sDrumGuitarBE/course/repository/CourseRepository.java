ac52c85dd83817218cf3ba7417d958593a97f8fb
seizethedaylyn <rlacofls294@gmail.com>
2026-02-20 16:11:11 +0900

    feat: get remainig lessons 몇회차 들을 수 있는지 get하는 메소드

--- src/main/java/teamDBMS/sDrumGuitarBE/course/repository/CourseRepository.java
+++ src/main/java/teamDBMS/sDrumGuitarBE/course/repository/CourseRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import teamDBMS.sDrumGuitarBE.course.entity.Course;
 
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+        Optional<Course> findTopByStudentIdAndStatusOrderByCreatedAtDesc(
+                Long studentId,
+                Course.EnrollmentStatus status
+        );
+
 }
